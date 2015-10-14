bd2ae72efc5c9b005c7a40efcbc395e4d0c5260e
jessica.rodriguez <>
2015-10-14 15:54:32 -0500

    * Contenido.cs: Agrego etiqueta de acceso exitoso
    * Login.cs: Enlace a contenido

--- Actividad8/Actividad8/Contenido.cs
+++ Actividad8/Actividad8/Contenido.cs
@@ -7,8 +7,23 @@ namespace Actividad8
 	{
 		public Contenido ()
 		{
+			Label texto = new Label {
+				Text = "Acceso autorizado",
+				TextColor = Color.Silver
+			};
 
+			StackLayout stackLayout = new StackLayout
+			{
+				Children =
+				{
+					texto,
+				}
+				};
+
+
+			//Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
 		}
 	}
 }
-
